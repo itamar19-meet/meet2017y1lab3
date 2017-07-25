c151d41bbd4f00e3a876cdfc6f2953db544706a5
itamar19-meet <itamar19@meet.mit.edu>
2017-07-25 18:33:09 +0300

    Stopping here

---
+++ dd.py
@@ -0,0 +1,30 @@
+
+print("what is your name")
+name = input()
+print("hello " + name)
+
+print("i can't wait to start writing harder programs! ")
+
+print("Who is your favorite instructor?")
+instructor = input()
+print("my favorite instructor is " + instructor + " too he is AWESOME.")
+print("Which team are you on?")
+team = input() 
+      
+
+print("group " + team + " is the best team in all of MEET")
+
+
+print("what is your age? ")
+
+age = input()
+if int(age) > 50:
+    print("this is too old man")
+
+print("What is your age?")
+age_2 = input()
+if int(age_2) > 50:
+    print("this is too old man")
+ages = int(age) + int(age_2)
+
+print("your ages together is " + str(ages))
